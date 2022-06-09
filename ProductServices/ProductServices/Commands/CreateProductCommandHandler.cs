99367216a1b108c533760c799549f4c5ec7f6e28
Huynh Le <lexuan.huynh@halliburton.com>
2022-06-09 19:48:00 +0700

    init repo

---
+++ ProductServices/ProductServices/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using ProductServices.DTOs;
+using ProductServices.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductServices.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, bool>
+    {
+        private readonly IProductRepository _productRepository;
+        public CreateProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<bool> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            return false;
+        }
+    }
+}
